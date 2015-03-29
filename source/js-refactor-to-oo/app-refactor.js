2cd330006e9878ac195cbae17d0a1019c679dd01
Ariel Fogel <fogeltine@gmail.com>
2015-03-28 17:07:22 -0700

    Add New Die to DOM implemented

--- source/js-refactor-to-oo/app-refactor.js
+++ source/js-refactor-to-oo/app-refactor.js
@@ -1,18 +1,49 @@
 ;
 
 // ------------------------------------
-// --------Controller------------
+// --------Controller------------------
 // ------------------------------------
-var DieController = function () {};
-
-DieController.prototype.bindEventListener = function () {
-  $('#roller button.add').on('click', function() {
-    $('.dice').append('<div class="die">0</div>');
-  });
+var DieController = function (view) {
+  this.view = view;
 };
 
+DieController.prototype.bindEventListener = function () {
+  var controller = this;
+  $('#roller button.add').on('click', this.addNewDie.bind(controller));
+};
+
+DieController.prototype.addNewDie = function () {
+  var die = new DieModel();
+  var dieTemplate = this.view.createDieTemplate(die.value);
+  this.view.renderDieToContainer(dieTemplate);
+}
+
+// ------------------------------------
+// --------View------------------------
+// ------------------------------------
+var DieView = function () {};
+
+DieView.prototype.renderDieToContainer = function (dieTemplate) {
+  $('.dice').append(dieTemplate);
+};
+
+DieView.prototype.createDieTemplate = function (dieValue) {
+  return '<div class="die">' + dieValue + '</div>';
+};
+
+// ------------------------------------
+// --------Model-----------------------
+// ------------------------------------
+
+var DieModel = function () {
+  this.value = 0;
+};
+
+// -------------------------------
 
 $(document).ready(function () {
-  var dieController = new DieController();
+  var dieController = new DieController(new DieView());
   dieController.bindEventListener();
 });
+
+
